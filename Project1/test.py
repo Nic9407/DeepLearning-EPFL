a63ee62eeb9ca5afefdbd13cc54b26770599c388
nicolò macellari <macenike@hotmail.it>
2020-04-01 13:02:01 +0200

    first commit

---
+++ Project1/test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 12:37:47 2020
+
+@author: physio
+"""
+import torch
+import dlc_practical_prologue as prologue
+
+
+# Generating test and training set
+train_input, train_target, train_classes,\
+    test_input,test_target, test_classes = prologue.generate_pair_sets(1000)
+    
+    
+    
+    
+    
+def main():
+    return 'tbi'
+
+
+if __name__ == "__main__":
+    main()
