43ef1935b795cf19c9decf38bdcf60058729949a
Andrej Janchevski <janchevski.andrej@gmail.com>
2020-05-13 14:49:22 +0200

    Fix error with usage of batch_norm when mini_batch_size=1

--- Project1/models.py
+++ Project1/models.py
@@ -17,34 +17,35 @@ class PairModel(nn.Module):
 
         self.conv1 = nn.Conv2d(2, nbch1, 3)
         self.maxp1 = nn.MaxPool2d(2)
-        if batch_norm:
+        if batch_norm and mini_batch_size > 1:
             self.bn1 = nn.BatchNorm2d(nbch1)
         if skip_connections:
             self.skip1 = nn.Linear(2 * 14 * 14, nbch1 * 6 * 6)
         self.conv2 = nn.Conv2d(nbch1, nbch2, 6)
-        if batch_norm:
+        if batch_norm and mini_batch_size > 1:
             self.bn2 = nn.BatchNorm2d(nbch2)
         if skip_connections:
             self.skip2 = nn.Linear(2 * 14 * 14, nbch2 * 1 * 1)
         self.fc1 = nn.Linear(nbch2, nbfch)
-        if batch_norm:
+        if batch_norm and mini_batch_size > 1:
             self.bn3 = nn.BatchNorm1d(nbfch)
         self.fc2 = nn.Linear(nbfch, 2)
 
     # Forward method
     def forward(self, x):
+        mini_batch_size = x.size(0)
         y = F.relu(F.max_pool2d(self.conv1(x), 2))
-        if self.batch_norm:
+        if self.batch_norm and mini_batch_size > 1:
             y = self.bn1(y)
         if self.skip_connections:
-            y += F.relu(self.skip1(x.view(x.size(0), 2 * 14 * 14)).view(y.size()))
+            y += F.relu(self.skip1(x.view(mini_batch_size, 2 * 14 * 14)).view(y.size()))
         y = F.relu(self.conv2(y))
-        if self.batch_norm:
+        if self.batch_norm and mini_batch_size > 1:
             y = self.bn2(y)
         if self.skip_connections:
-            y += F.relu(self.skip2(x.view(x.size(0), 2 * 14 * 14)).view(y.size()))
+            y += F.relu(self.skip2(x.view(mini_batch_size, 2 * 14 * 14)).view(y.size()))
         y = F.relu(self.fc1(y.view(-1, self.nbch2)))
-        if self.batch_norm:
+        if self.batch_norm and mini_batch_size > 1:
             y = self.bn3(y)
         y = F.relu(self.fc2(y))
         return y
@@ -62,35 +63,36 @@ class SiameseModel(nn.Module):
         self.skip_connections = skip_connections
 
         self.conv1 = nn.Conv2d(1, nbch1, 3)
-        if batch_norm:
+        if batch_norm and mini_batch_size > 1:
             self.bn1 = nn.BatchNorm2d(nbch1)
         if skip_connections:
             self.skip1 = nn.Linear(1 * 14 * 14, nbch1 * 6 * 6)
         self.conv2 = nn.Conv2d(nbch1, nbch2, 6)
-        if batch_norm:
+        if batch_norm and mini_batch_size > 1:
             self.bn2 = nn.BatchNorm2d(nbch2)
         if skip_connections:
             self.skip2 = nn.Linear(1 * 14 * 14, nbch2 * 1 * 1)
         self.fc1 = nn.Linear(nbch2, nbfch)
-        if self.batch_norm:
+        if self.batch_norm and mini_batch_size > 1:
             self.bn3 = nn.BatchNorm1d(nbfch)
         self.fc2 = nn.Linear(nbfch, 10)
         self.fc3 = nn.Linear(20, 2)
 
     # Method implementing the forward pass of one of the parallel branches of the siamese network
     def __forward_one_branch(self, x):
+        mini_batch_size = x.size(0)
         y = F.relu(F.max_pool2d(self.conv1(x), 2))
-        if self.batch_norm:
+        if self.batch_norm and mini_batch_size > 1:
             y = self.bn1(y)
         if self.skip_connections:
-            y += F.relu(self.skip1(x.view(x.size(0), 1 * 14 * 14)).view(y.size()))
+            y += F.relu(self.skip1(x.view(mini_batch_size, 1 * 14 * 14)).view(y.size()))
         y = F.relu(self.conv2(y))
-        if self.batch_norm:
+        if self.batch_norm and mini_batch_size > 1:
             y = self.bn2(y)
         if self.skip_connections:
-            y += F.relu(self.skip2(x.view(x.size(0), 1 * 14 * 14)).view(y.size()))
+            y += F.relu(self.skip2(x.view(mini_batch_size, 1 * 14 * 14)).view(y.size()))
         y = F.relu(self.fc1(y.view(-1, self.nbch2)))
-        if self.batch_norm:
+        if self.batch_norm and mini_batch_size > 1:
             y = self.bn3(y)
         y = F.relu(self.fc2(y))
         return y
