ca96fcf70ce1e45c73aeaa7830aa1eecf2f11bfe
Andrej Janchevski <janchevski.andrej@gmail.com>
2020-05-13 14:53:09 +0200

    Fix incorrect standard deviations and improve plots

--- Project1/plot.py
+++ Project1/plot.py
@@ -23,9 +23,7 @@ def visualize_cross_validation_results(cross_val_results, plots_filepath):
         data = []
         for param_gropby_levels in ((0,), (1,), (2,), (3, 4), (5,)):
             aggregate_scores = scores.groupby(level=param_gropby_levels).mean()
-            # Std[Mean(X)] = sqrt(Var[Sum(X) / N]) = sqrt(Sum(Var[X]) / N^2) = sqrt(Sum(Std[X]^2)) / N
-            n = scores.shape[0]
-            aggregate_stds = stds.groupby(level=param_gropby_levels).apply(lambda x: x.pow(2).sum().pow(0.5) / n)
+            aggregate_stds = scores.groupby(level=param_gropby_levels).std()
             data.append((aggregate_scores, aggregate_stds))
         return data
 
@@ -34,7 +32,7 @@ def visualize_cross_validation_results(cross_val_results, plots_filepath):
     siamese_model_10_data = aggregate_results(siamese_model_scores_10, siamese_model_stds_10)
 
     # Group results for all models
-    model_names = ("PAIR", "SIAMESE 2", "SIAMESE 10")
+    model_names = ("Pair", "Siamese 2", "Siamese 10")
     grouped_data = []
     for pair_model_group_data, siamese_model_2_group_data, siamese_model_10_group_data in zip(pair_model_data,
                                                                                               siamese_model_2_data,
@@ -49,19 +47,19 @@ def visualize_cross_validation_results(cross_val_results, plots_filepath):
 
         grouped_data.append((score_mean_data, score_std_data))
 
-    plots_param_names = ("nbch1", "nbch2", "nbfch", "batch_norm,skip_con", "lr")
-    for plot_param_names, (score_mean_data, score_std_data) in zip(plots_param_names, grouped_data):
+    plots_param_names = ("nbch1", "nbch2", "nbfch", "batch_norm+skip_con", "lr")
+    for i, (plot_param_names, (score_mean_data, score_std_data)) in enumerate(zip(plots_param_names, grouped_data)):
         plt.figure(figsize=(10, 5))
-        score_mean_data.transpose().plot.bar(yerr=score_std_data.transpose(),
-                                             capsize=5,
-                                             ylim=(0.5, 1),
-                                             colormap="gist_rainbow")
+        score_mean_data.plot.line(yerr=score_std_data,
+                                  capsize=5,
+                                  ylim=(0.4, 1.1),
+                                  colormap=colormap_brg_darker)
         plt.title("Cross validation results for parameters:\n{}".format(plot_param_names), fontsize=18)
-        plt.xlabel("Model", fontsize=14)
+        plt.xlabel("Parameter value", fontsize=14)
         plt.ylabel("Average accuracy", fontsize=14)
         plt.xticks(fontsize=12, rotation=30)
         plt.yticks(fontsize=12)
-        plt.legend(title="Parameter values", title_fontsize=10)
+        plt.legend(title="Model", title_fontsize=10)
         plt.tight_layout()
         plt.savefig(fname=plots_filepath + "cross_validation_{}.png".format(plot_param_names),
                     dpi="figure", format="png")
@@ -82,7 +80,7 @@ def visualize_gradient_norms(gradient_norms, parameter_names, plot_filepath):
         ax.tick_params(axis="x", labelsize=10)
         ax.tick_params(axis="y", labelsize=10)
         grad_norms.plot.line(ax=ax, figsize=(10, 5),
-                             colormap="gist_rainbow",
+                             colormap="plasma_r",
                              logy=True,
                              ylim=(1e-6, 1e6))
         ax.legend(title="Depth", prop={"size": 6}, title_fontsize=8)
